3d250370f22dc0d17e45ec716dccdd0d5322a11a
Gabriel Decima <gabrieldecima1028@gmail.com>
2022-07-07 23:31:13 -0300

    Algoritmo creado, uso de For + If

---
+++ main.js
@@ -0,0 +1,27 @@
+let promedio
+let nota =0;
+let nombreAgente = prompt("Ingresa el Nombre del Agente");
+let notaMonitoreo
+
+for(let i=1;i<=3;i++){
+
+    notaMonitoreo = parseInt(prompt("Ingresa la nota URA " + nombreAgente));
+    nota = notaMonitoreo + nota ;
+
+             
+}
+promedio = nota /3;
+
+if (promedio>85){
+    alert("Tu promedio es " + promedio + " Estas en Objetivo, Gran trabajo!");
+
+}else{
+    alert("Tu promedio es " + promedio + " No alcanzaste el objetivo, tenemos que mejorar");
+}
+
+
+   
+
+
+
+
