0c9f418d186e57f7dd39fdb5c07ecdca08a652d9
Gabriel Decima <gabrieldecima@gmail.com>
2022-08-06 13:20:58 -0300

    Modificación JS, EVENTOS, AGREGAR CARRITO

--- js/scripts.js
+++ js/scripts.js
@@ -29,34 +29,6 @@
 
 
 
-
-// alert("Vamos a realizar una compra!!");
-
-
-// while(pregunta !="NO") { 
-
-// const producto1 = { id:"1", marca: "adidas",talle: "46"}
-// const producto2 = {id:"2" ,marca:"puma",talle:"42"}
-// const producto3 = {id:"3" ,marca:"Nike",talle:"44"}
-// const producto4 = {id:"4" ,marca:"Loto",talle:"38"}
-
-
-// carrito.push(prompt("Selecciona el producto,Tenemos disponibles las sigiuentes Zapatillas: " + (producto1.marca) + " - " + (producto2.marca) + " - " + (producto3.marca) + " - " + (producto4.marca)));
-
-// pregunta = prompt("¿Quieres seguir comprando? (Ingresa SI - NO)");
-
-// }
-
-// alert("Gracias por confiar en nosotros " + nombreUsuario);
-// alert ("Añadiste al carrito las siguientes Zapas : " + carrito + " En total tenes " + carrito.length + " Productos") ;
-
-
-
-// 
-
-
-
-
 const carrito = [];
 
 
