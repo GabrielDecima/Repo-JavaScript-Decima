30a5aa52e4f75648355a4b6f1bbda1baf596aa83
Gabriel Decima <gabrieldecima@gmail.com>
2022-08-19 13:07:40 -0300

    Modificando estilos de pagina

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,17 @@ const carrito = [];
 
 
 const productos = [
-   { id:"1", marca: "adidas",precio:10000 ,talle: "46"},
-   {id:"2" ,marca:"puma",precio:8000 ,talle:"42"},
-   {id:"3" ,marca:"Nike",precio:15000 ,talle:"44",img:""},
-   {id:"4" ,marca:"Loto",precio:4000 ,talle:"38"},
+   { id:"1", marca: "Adidas",precio:10000 ,talle: "46", img:"https://www.stockcenter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwad70818c/products/AD_FX3581/AD_FX3581-1.JPG"},
+   {id:"2" ,marca:"Puma",precio:8000 ,talle:"42", img:"https://www.stockcenter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw1686a8f4/products/PU_195648-01/PU_195648-01-1.JPG"},
+   {id:"3" ,marca:"Nike",precio:15000 ,talle:"44",img:"https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwa6297e94/products/NI_DA2776-008/NI_DA2776-008-1.JPG"},
+   {id:"4" ,marca:"Loto",precio:4000 ,talle:"38", img:"https://www.stockcenter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw25b06e25/products/LO_SS22C0002/LO_SS22C0002-1.JPG"},
    ];
 
 
   productos.forEach((producto) => {
    const idButton = `add-cart${producto.id}`
-   document.getElementById("espacio-card").innerHTML += `<div class="card h-100 d-flex m-4">
-   <img class="card-img-top" src="${producto.img}" alt="..." />
+   document.getElementById("espacio-card").innerHTML += `<div class="card h-10 d-flex m-4">
+   <img class="card-img-top " src="${producto.img}" alt="..." />
    <div class="card-body p-4">
   <div class="text-center">
   <h5 class="fw-bolder">${producto.marca}</h5>
@@ -26,10 +26,6 @@ const productos = [
   </div>`;
 
 })
-
-
-
-
 
 productos.forEach((producto) => {
    const idButton = `add-cart${producto.id}`
