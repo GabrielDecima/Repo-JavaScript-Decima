ed5db6b6ad1bb8622aac00dcda0a4e93ccec33ae
Gabriel Decima <gabrieldecima@gmail.com>
2022-08-30 21:46:15 -0300

    Modificando Fetch

--- js/scripts.js
+++ js/scripts.js
@@ -3,33 +3,22 @@
 const carrito = [];
 
 
-
-// const productos = [
-//    { id:"1", marca: "Adidas",precio:10000 ,talle: "46", img:"https://www.stockcenter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwad70818c/products/AD_FX3581/AD_FX3581-1.JPG"},
-//    {id:"2" ,marca:"Puma",precio:8000 ,talle:"42", img:"https://www.stockcenter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw1686a8f4/products/PU_195648-01/PU_195648-01-1.JPG"},
-//    {id:"3" ,marca:"Nike",precio:15000 ,talle:"44",img:"https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwa6297e94/products/NI_DA2776-008/NI_DA2776-008-1.JPG"},
-//    {id:"4" ,marca:"Loto",precio:4000 ,talle:"38", img:"https://www.stockcenter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw25b06e25/products/LO_SS22C0002/LO_SS22C0002-1.JPG"},
-//    ];
-
 const consultarDatosProducto = () => {
    fetch('https://api.mercadolibre.com/sites/MLA/search?q=zapatillas')
    .then((response) => response.json())
    .then(informacion => {
-      let acumuladorCards = ``;
-      informacion.results.forEach((producto) => {
-      const idButton = `add-cart${producto.id}`
-   acumuladorCards +=  
-      
-   `<div class ="card">
-   <img src="${producto.thumbnail}">
-   <span>
-  <h5 class="fw-bolder">${producto.title}</h5>
-  <span/>
- <h2>$ ${producto.price} </h2>
- <a class="boton" id="${idButton}" data-id="${producto.id}">Agregar al Carrito</a> 
-  
-  </div>`;
+   let acumuladorCards = ``;
+   informacion.results.forEach((producto) => {
 
+   console.log(informacion.results);
+   const idButton = `add-cart${producto.id}`
+   acumuladorCards +=  `<div class= "card">
+   <img src="${producto.thumbnail}">
+   <h2>${producto.title}</h2>
+   <h2>${producto.price}</h2>
+   <a class="boton" id=${idButton}>Agregar</a>
+   </div>`
+   
 })
    document.getElementById('espacio-card').innerHTML = acumuladorCards;
    
