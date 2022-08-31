afa5afbd409609062f7f4c3e54b027cbe4f249ee
Gabriel Decima <gabrieldecima@gmail.com>
2022-08-30 21:25:49 -0300

    Agregando Fetch

--- js/scripts.js
+++ js/scripts.js
@@ -4,30 +4,43 @@ const carrito = [];
 
 
 
-const productos = [
-   { id:"1", marca: "Adidas",precio:10000 ,talle: "46", img:"https://www.stockcenter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwad70818c/products/AD_FX3581/AD_FX3581-1.JPG"},
-   {id:"2" ,marca:"Puma",precio:8000 ,talle:"42", img:"https://www.stockcenter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw1686a8f4/products/PU_195648-01/PU_195648-01-1.JPG"},
-   {id:"3" ,marca:"Nike",precio:15000 ,talle:"44",img:"https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwa6297e94/products/NI_DA2776-008/NI_DA2776-008-1.JPG"},
-   {id:"4" ,marca:"Loto",precio:4000 ,talle:"38", img:"https://www.stockcenter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw25b06e25/products/LO_SS22C0002/LO_SS22C0002-1.JPG"},
-   ];
+// const productos = [
+//    { id:"1", marca: "Adidas",precio:10000 ,talle: "46", img:"https://www.stockcenter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwad70818c/products/AD_FX3581/AD_FX3581-1.JPG"},
+//    {id:"2" ,marca:"Puma",precio:8000 ,talle:"42", img:"https://www.stockcenter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw1686a8f4/products/PU_195648-01/PU_195648-01-1.JPG"},
+//    {id:"3" ,marca:"Nike",precio:15000 ,talle:"44",img:"https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwa6297e94/products/NI_DA2776-008/NI_DA2776-008-1.JPG"},
+//    {id:"4" ,marca:"Loto",precio:4000 ,talle:"38", img:"https://www.stockcenter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw25b06e25/products/LO_SS22C0002/LO_SS22C0002-1.JPG"},
+//    ];
 
-
-  productos.forEach((producto) => {
-   const idButton = `add-cart${producto.id}`
-   document.getElementById("espacio-card").innerHTML += `<div class="card h-10 d-flex m-4">
-   <img class="card-img-top " src="${producto.img}" alt="..." />
-   <div class="card-body p-4">
-  <div class="text-center">
-  <h5 class="fw-bolder">${producto.marca}</h5>
-  <p>Talle ${producto.talle}</p>
-   <div>$ ${producto.precio} </div>
-  <a class="boton" id="${idButton}" data-id="${producto.id}">Agregar al Carrito</a>
-  </div>
+const consultarDatosProducto = () => {
+   fetch('https://api.mercadolibre.com/sites/MLA/search?q=zapatillas')
+   .then((response) => response.json())
+   .then(informacion => {
+      let acumuladorCards = ``;
+      informacion.results.forEach((producto) => {
+      const idButton = `add-cart${producto.id}`
+   acumuladorCards +=  
+      
+   `<div class ="card">
+   <img src="${producto.thumbnail}">
+   <span>
+  <h5 class="fw-bolder">${producto.title}</h5>
+  <span/>
+ <h2>$ ${producto.price} </h2>
+ <a class="boton" id="${idButton}" data-id="${producto.id}">Agregar al Carrito</a> 
+  
   </div>`;
 
 })
+   document.getElementById('espacio-card').innerHTML = acumuladorCards;
+   
+   })
+}
 
-productos.forEach((producto) => {
+consultarDatosProducto();
+
+
+
+informacion.results.forEach((producto) => {
    const idButton = `add-cart${producto.id}`
    document.getElementById(idButton).addEventListener('click', () =>{
       carrito.push(producto);
@@ -41,48 +54,69 @@ productos.forEach((producto) => {
    })
        
 })
+  
+   
 
 
-function log (){
 
 
-let nombreUsuario="";
+
+
+
+
+// function log (){
+
+
+// let nombreUsuario="";
    
    
-while(nombreUsuario == ""){
+// while(nombreUsuario == ""){
 
-    nombreUsuario= prompt ("Ingrese nombre de usuario");
+//     nombreUsuario= prompt ("Ingrese nombre de usuario");
     
 
-    if (nombreUsuario != ""){
+//     if (nombreUsuario != ""){
 
-      const Toast = Swal.mixin({
-         toast: true,
-         position: 'top-end',
-         showConfirmButton: false,
-         timer: 3000,
-         timerProgressBar: true,
-         didOpen: (toast) => {
-           toast.addEventListener('mouseenter', Swal.stopTimer)
-           toast.addEventListener('mouseleave', Swal.resumeTimer)
-         }
-       })
+//       const Toast = Swal.mixin({
+//          toast: true,
+//          position: 'top-end',
+//          showConfirmButton: false,
+//          timer: 3000,
+//          timerProgressBar: true,
+//          didOpen: (toast) => {
+//            toast.addEventListener('mouseenter', Swal.stopTimer)
+//            toast.addEventListener('mouseleave', Swal.resumeTimer)
+//          }
+//        })
        
-       Toast.fire({
-         icon: 'success',
-         title: 'Bienvenido'+ nombreUsuario
-       })
+//        Toast.fire({
+//          icon: 'success',
+//          title: 'Bienvenido'+ nombreUsuario
+//        })
       
       
       
-}
- else {
-    alert("No ingresaste un nombre, por favor volve, e ingresa un nombre");
+// }
+//  else {
+//     alert("No ingresaste un nombre, por favor volve, e ingresa un nombre");
    
- }
-}
+//  }
+// }
 
-}
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
